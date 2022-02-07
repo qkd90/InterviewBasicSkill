f5df5e2eed930ec811e42bd23ec7bbdb9258b776
qiangren <qrenchina@163.com>
2022-02-07 11:09:34 +0800

    2-7

--- 剑指offer专项突击版/test.java
+++ 剑指offer专项突击版/test.java
@@ -1,22 +1,17 @@
-public class test{
-    public static void main(String args[]){
-      String s = "race a car";
-      System.out.println(isPalindrome(s));  
-    }
-
-    public static boolean isPalindrome(String s) {
-        StringBuffer sletter = new StringBuffer();
-        int length = s.length();
-        for (int i = 0; i < length; i++) {
-            char ch = s.charAt(i);
-            if (Character.isLetterOrDigit(ch)) {
-                sletter.append(Character.toLowerCase(ch));
+class Solution {
+    public boolean validPalindrome(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right) {
+            if (++left == --right){
+                return true;
             }
+            if ((s.charAt(left) != s.charAt(right)) && (s.charAt(++left) != s.charAt(right)) && (s.charAt(left) != s.charAt(--right))) {
+                return false;
+            }
+            ++left;
+            --right;
         }
-        StringBuffer letters = new StringBuffer(sletter).reverse();
-        if(letters.toString().equals(sletter.toString())){
-            return true;
-        }
-        return false;
+        return true;
     }
-  }
+}
