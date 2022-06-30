8cd4228e456b40ad616a1bac1f0467c9d6900e4f
qiangren <qrenchina@163.com>
2022-06-30 19:25:18 +0800

    06-30

--- LeetCode 热题 HOT 100/test.java
+++ LeetCode 热题 HOT 100/test.java
@@ -1,28 +1,17 @@
-class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) {
-            return "";
-        }
-        int start = 0, end = 0;
-        for (int i = 0; i < s.length(); i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
-            int len = Math.max(len1, len2);
-            if (len > end - start) {
-                start = i - (len - 1) / 2;
-                end = i + len / 2;
+public class Solution {
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length - 1;
+        int ans = 0;
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            ans = Math.max(ans, area);
+            if (height[left] <= height[right]) {
+                ++left;
+            }
+            else {
+                --right;
             }
         }
-        return s.substring(start, end + 1);
-    }
-
-    public int expandAroundCenter(String s, int left, int right) {
-        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
-            --left;
-            ++right;
-        }
-        return right - left - 1;
+        return ans;
     }
 }
-
-
